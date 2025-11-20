1a0caf48ad38fe34f5a95fb5a02d09e687677990
rafaelalonso153306 <rafaelalonso153306@gmail.com>
2025-11-19 21:01:18 -0300

    crando os controllers

--- obj/Debug/net9.0/Projetositedeestudo.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projetositedeestudo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projetositedeestudo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6d929871fbe8d25caf10a9ca44d073b43c0bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac070e54616307f38b678feb5099a00f740347ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projetositedeestudo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projetositedeestudo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
